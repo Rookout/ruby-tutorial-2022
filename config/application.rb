0a4a5788efa6ded623f4f99dd67d148dba6fdd05
Urook <uri@rookout.com>
2022-07-06 15:35:59 +0300

    Start Rookout

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative "boot"
 
 require "rails/all"
+require "rookout"
+::Rookout.start token: "XXXXXXXXXXXXXXXX", labels: {env: "dev"}
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
